7c549255af6d0c9cae5ab439828398ab4b847c27
mac <mac@macdemac-mini.local>
2016-07-27 14:37:02 +0800

    创建项目

---
+++ Yunjia02/Yunjia02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yunjia02
+//
+//  Created by mac on 16/7/27.
+//  Copyright © 2016年 ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
